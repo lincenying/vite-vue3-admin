13c7bb315f25b009bc0060c1f6465cf43cdca82a
林岑影 <lincenying@qq.com>
2023-08-01 16:50:03 +0800

    chore: update vite.config

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -4,10 +4,7 @@ import type { BuildOptions, ServerOptions } from 'vite'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-const config: {
-    server: ServerOptions
-    build: BuildOptions
-} = {
+const config: { server: ServerOptions; build: BuildOptions } = {
     server: {
         port: 3001,
         host: '0.0.0.0',
