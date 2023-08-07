a3c11d488895113e1cec239d35cb06cca2b0b349
林岑影 <lincenying@qq.com>
2023-08-07 16:53:25 +0800

    chore: 各配置文件注释& update deps

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,11 +1,4 @@
-import {
-    defineConfig,
-    presetAttributify,
-    presetIcons,
-    presetUno,
-    transformerDirectives,
-    transformerVariantGroup,
-} from 'unocss'
+import { defineConfig, presetAttributify, presetIcons, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss'
 
 export default defineConfig({
     shortcuts: [
@@ -21,13 +14,40 @@ export default defineConfig({
         ['line-4', 'text-truncate line-clamp-4'],
     ],
     presets: [
+        /**
+         * 默认预设
+         * @see https://unocss.dev/presets/uno
+         */
         presetUno(),
+        /**
+         * 开启属性模式
+         * @see https://unocss.dev/presets/attributify
+         * @example <div text="sm white" font="mono light"></div>
+         */
         presetAttributify(),
+        /**
+         * 开启自定义图标模式
+         * @see https://unocss.dev/presets/icons
+         * @example <div i-<collection>-<icon></div>
+         */
         presetIcons({
-            /* options */
+            prefix: 'i-',
         }),
     ],
-    transformers: [transformerDirectives(), transformerVariantGroup()],
+    transformers: [
+        /**
+         * 启用 --uno: 功能
+         * @see https://unocss.dev/transformers/directives
+         * @example .custom-div { --uno: text-center my-0 font-medium; }
+         */
+        transformerDirectives(),
+        /**
+         * 启用 () 分组功能
+         * @see https://unocss.dev/transformers/variant-group
+         * @example <div class="hover:(bg-gray-400 font-medium) font-(light mono)"/>
+         */
+        transformerVariantGroup(),
+    ],
     safelist: 'svg-text1 svg-text2'.split(' '),
     rules: [],
 })
