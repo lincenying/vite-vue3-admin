7b659e4531c2de0f164923bb41558b75c84fe226
林岑影 <lincenying@qq.com>
2023-08-16 18:11:01 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts" setup>
-import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import LayoutIndex from './layout/index.vue'
 
 defineOptions({
