07ad7bcce8fb6f30d4d88f2ea1bb5d0bc81ab59f
LinCenYing <lincenying@qq.com>
2025-09-25 14:24:02 +0800

    chore: update deps

--- src/shims-types.d.ts
+++ src/shims-types.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="@lincy/utils" />
 /// <reference types="vite/client" />
-/// <reference types="unplugin-vue-macros/macros-global" />
+/// <reference types="vue-macros/macros-global" />
 
 export {}
